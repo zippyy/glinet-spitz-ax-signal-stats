4e54eabea01506c688130eb9318c0ce263cd8a8a
Raif Atef <beliskner.github.3psil0n@neverbox.com>
2025-07-27 18:22:06 +0300

    Add response microcache to the handler

--- main.go
+++ main.go
@@ -8,9 +8,17 @@ import (
 	"log"
 	"os/exec"
 	"strings"
+	"sync"
+	"time"
 
 	"go.oneofone.dev/gserv"
 )
+
+const cacheDuration = 500 * time.Millisecond
+
+var cacheMutex sync.Mutex
+var cachedResponse gserv.Response
+var lastCacheTime time.Time
 
 func main() {
 	// Define a variable to hold the port number
@@ -33,7 +41,7 @@ func start(port int) {
 	srv := gserv.New()
 
 	svc := Svc{}
-	srv.GET("/", svc.HandleRequest)
+	srv.GET("/", CreateCacheHandler(svc.HandleRequest))
 
 	fmt.Printf("Listening on port %d. Press CTLR+C to exit...\n", port)
 	log.Panic(srv.Run(context.Background(), "0.0.0.0:"+fmt.Sprintf("%d", port)))
@@ -87,5 +95,22 @@ func execCommandAndGetResponse(command string, args ...string) string {
 		output = strings.Replace(output, "\r\nOK\r\n", "", 1)
 		output = strings.Trim(output, "\r\n \t")
 		return "<pre>" + output + "</pre>"
+	}
+}
+
+func CreateCacheHandler(handler func(*gserv.Context) gserv.Response) func(*gserv.Context) gserv.Response {
+	return func(ctx *gserv.Context) gserv.Response {
+		cacheMutex.Lock()
+		defer cacheMutex.Unlock()
+
+		if time.Since(lastCacheTime) < cacheDuration && cachedResponse != nil {
+			return cachedResponse
+		}
+
+		newResponse := handler(ctx)
+		cachedResponse = newResponse
+		lastCacheTime = time.Now()
+
+		return newResponse
 	}
 }
